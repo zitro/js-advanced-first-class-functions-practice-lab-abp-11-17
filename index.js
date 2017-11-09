c82f835dd3423e90d7815bdc2192456f9fb7d3e2
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-09 15:22:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+
+const logDriverNames = function (drivers) {
+  drivers.forEach(function (array) {
+    console.log(array.name);
+  });
+};
+const logDriversByHometown = function (driver, location) {
+  driver.forEach(function (array) {
+    console.log(array.name);
+  });
+};
+
+var result = [];
+array.reduce(function (res, value) {
+    if (!res[value.Id]) {
+        res[value.Id] = {
+            qty: 0,
+            Id: value.Id
+        };
+        result.push(res[value.Id])
+    }
+    res[value.Id].qty += value.qty
+    return res;
+}, {});
