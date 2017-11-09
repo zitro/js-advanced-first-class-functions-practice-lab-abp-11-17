548b05fee1834d3975605928792a281a43bd79db
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-09 17:35:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,5 +34,5 @@ const totalRevenue = function (driver) {
 
   const averageRevenue = function (driver) {
     return driver.reduce(function (total, array) {
-      return (total + array.revenue / driver.length();}, 0);
+      return (total + array.revenue / driver.length());}, 0);
     };
