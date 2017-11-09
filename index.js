275038a7cb1cba81ea7f3af54b68f4fd8c008f8a
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-09 16:40:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 // Code your solution in this file!
 
-const logDriverNames = function (drivers) {
-  drivers.forEach(function (array) {
+const logDriverNames = function (driver) {
+  driver.forEach(function (array) {
     console.log(array.name);
   });
 };
 
-const logDriversByHometown = function (drivers, hometown) {
-  drivers.forEach(function (array) {
+const logDriversByHometown = function (driver, hometown) {
+  driver.forEach(function (array) {
     if(array.hometown === hometown){
       console.log(array.name);}
   });
 };
+
+const driversByRevenue = function (driver){
+
+  
+}
