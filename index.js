fa6645161c7c14e170a7be9e3d8717bfdc7561b2
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-09 15:41:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,27 +14,3 @@ const logDriversByHometown = function (driver, location) {
 
 
 
-
-
-var result = [];
-array.reduce(function (res, value) {
-    if (!res[value.Id]) {
-        res[value.Id] = {
-            qty: 0,
-            Id: value.Id
-        };
-        result.push(res[value.Id])
-    }
-    res[value.Id].qty += value.qty
-    return res;
-}, {});
-
-const getTotalRevenue = function (products) {
-  let totalPrice = 0;
-
-  products.forEach(function (product) {
-    totalPrice += product.price;
-  });
-
-  return totalPrice;
-};
